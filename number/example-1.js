a8d06fbbc374a792ed52433b6215aa80b61b6ad5
Ederson Macedo <ederson.mos@gmail.com>
2022-12-30 16:31:55 -0300

    Created example typeof number
    Create modes possible decimal, hexadecimal, binary and octal

---
+++ number/example-1.js
@@ -0,0 +1,14 @@
+console.log(10);
+// expect output (decimal): 10
+
+console.log(9.9);
+// expect output (decimal): 9.9
+
+console.log(0xFF);
+// expect output (hexadecimal): 10
+
+console.log(0b10);
+// expect output (binary): 2
+
+console.log(0o10);
+// expect output (octal): 8

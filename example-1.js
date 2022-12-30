f31fb3da83bd2e932ae3b735cd4498b7117bd117
Ederson Macedo <ederson.mos@gmail.com>
2022-12-30 10:04:07 -0300

    Created example about existing data types

---
+++ example-1.js
@@ -0,0 +1,18 @@
+console.log(typeof 10);
+//expect output: 'number'
+
+console.log(typeof "JavaScript");
+// expect output: 'string'
+
+console.log(typeof true);
+// expect output: 'boolean'
+
+console.log(typeof Symbol("iterator"));
+// expect output: 'symbol'
+
+console.log(typeof null);
+// expect output: 'object'
+
+console.log(typeof undefined);
+// expect  output: 'undefined'
+

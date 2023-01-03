d4c1fb4fc226cd14815eb16673d251944e619fc5
Ederson Macedo <ederson.mos@gmail.com>
2023-01-03 16:06:29 -0300

    Created example about simple operators

---
+++ number-operator/example-1.js
@@ -0,0 +1,14 @@
+console.log(2 + 2);
+//expect output: 4
+
+console.log(8 - 3);
+// expect output: 5
+
+console.log(4 * 5);
+// expect output: 20
+
+console.log(9 / 3);
+// expect output: 3
+
+console.log(7 % 2);
+// expect output: 1

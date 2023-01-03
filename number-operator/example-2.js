bb839d898f680223e4622fa790927bf1aa59a4a1
Ederson Macedo <ederson.mos@gmail.com>
2023-01-03 17:01:42 -0300

    Created example of assigment operator

---
+++ number-operator/example-2.js
@@ -0,0 +1,24 @@
+let result = 10;
+console.log(result);
+// expect output: 10
+
+result += 2;
+console.log(result);
+// expect output: 12
+
+result -= 5;
+console.log(result);
+// expect output: 7
+
+result *= 8;
+console.log(result);
+// expect output: 56
+
+result /= 2;
+console.log(result);
+// expect output: 28
+
+result %= 6;
+console.log(result);
+// expect output: 4 
+

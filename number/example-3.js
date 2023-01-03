a1ce6d7bb9509a37f58282c24ef75897f11ac289
Ederson Macedo <ederson.mos@gmail.com>
2023-01-03 11:39:20 -0300

    Created use metods of constructor

---
+++ number/example-3.js
@@ -0,0 +1,8 @@
+console.log((9876.5).toExponential(10));
+// expect output: '9.8765000000e+3'
+
+console.log((9876.5).toFixed(10));
+// expect output: '9876.5000000000'
+
+console.log((9876.5).toPrecision(10));
+// expect output: '9876.500000'

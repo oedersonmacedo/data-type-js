e5d0933d1b3f26ed6bae15a70f7de07196aa6ff8
Ederson Macedo <ederson.mos@gmail.com>
2023-01-09 08:14:09 -0300

    Created example about type conversion number with Number

---
+++ number-conversion/example-1.js
@@ -0,0 +1,21 @@
+console.log(Number("10"));
+// expect output: 10
+
+console.log(Number("9.9"));
+// expect output: 9.9
+
+console.log(Number("0xFF"));
+// expect output: 255
+
+console.log(Number("0b10"));
+// expect output: 2
+
+console.log(Number("0o10"));
+// expect output: 8
+
+console.log(Number());
+// expect output: 0
+
+console.log(Number("JavaScript"));
+// expect output: NaN
+

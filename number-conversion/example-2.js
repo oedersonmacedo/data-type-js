d8a8dc4cc6be8d9e822d70566e9e82d2fd18af55
Ederson Macedo <ederson.mos@gmail.com>
2023-01-09 08:18:14 -0300

    Create example for realize conversion through numeric operators

---
+++ number-conversion/example-2.js
@@ -0,0 +1,20 @@
+console.log(~~"10");
+// expect output: 10
+
+console.log(+"10");
+// expect output: 10
+
+console.log("10" - 0);
+// expect output: 10
+
+console.log("10" * 1);
+// expect output: 10
+
+console.log("10" / 1);
+// expect output: 10
+
+console.log("10" + 0);
+// expect output WARNING: '100'
+
+console.log(+"10" + 0);
+// expect output: 10

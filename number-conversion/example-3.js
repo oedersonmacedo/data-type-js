786cbf104f2af4b2ab3fe5e10341ebe291cf85a4
Ederson Macedo <ederson.mos@gmail.com>
2023-01-10 07:40:55 -0300

    Create example of to use toString in number

---
+++ number-conversion/example-3.js
@@ -0,0 +1,11 @@
+console.log((0xA).toString(10)); // Convert for Decimal
+// expect output: '10'
+
+console.log((0b1010).toString(16)); // Convert for Hexadecimal
+// expect output: 'a'
+
+console.log((010).toString(2)); // Convert for binary
+// expect output: '1000'
+
+console.log((10).toString(8)); // Convert for octal
+// expect output: '12'

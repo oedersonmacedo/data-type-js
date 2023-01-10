b0a7784d40df1fbb25c6ed1bcb21ad422ad439c1
Ederson Macedo <ederson.mos@gmail.com>
2023-01-10 07:53:05 -0300

    Created example of to use parseFloat

---
+++ number-conversion/example-5.js
@@ -0,0 +1,14 @@
+console.log(parseFloat("10"));
+// expect output: 10
+
+console.log(parseFloat("2.5"));
+// expect output: 2.5
+
+console.log(parseFloat("0xFF"));
+// expect output: 0
+
+console.log(parseFloat("0b10"));
+// expect output: 0
+
+console.log(parseFloat("b10"));
+// expect output: NaN

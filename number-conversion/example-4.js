034fc7407de86fc838201190e08212c9e426a456
Ederson Macedo <ederson.mos@gmail.com>
2023-01-10 07:46:37 -0300

    Created example of to use parseInt and its bases

---
+++ number-conversion/example-4.js
@@ -0,0 +1,14 @@
+console.log(parseInt("10", 10));
+// expect outupt: 10
+
+console.log(parseInt("9.9", 10));
+// expect outupt: 9
+
+console.log(parseInt("A", 16));
+// expect outupt: 10
+
+console.log(parseInt("11", 2));
+// expect outupt: 3
+
+console.log(parseInt("010", 8));
+// expect outupt: 8

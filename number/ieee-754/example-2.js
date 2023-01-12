835f9e3c8cdc9b8996687398820da93350596a23
Ederson Macedo <ederson.mos@gmail.com>
2023-01-12 06:51:53 -0300

    Created example of return Infinity

---
+++ number/ieee-754/example-2.js
@@ -0,0 +1,14 @@
+console.log(1/0);
+// expect output: Infinity
+
+console.log(Math.pow(10, 1000));
+// expect output: Infinity
+
+console.log(Number.MAX_VALUE * 2);
+// expect output: Infinity
+
+console.log(Math.log(0));
+// expect output: -Infinity
+
+console.log(-Number.MAX_VALUE * 2);
+// expect output: -Infinity

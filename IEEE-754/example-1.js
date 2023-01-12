b02aac3c93e6b0486b4effcfe5ddf727f1ee40cb
Ederson Macedo <ederson.mos@gmail.com>
2023-01-12 06:41:16 -0300

    Created example of residuals due to rouding

---
+++ IEEE-754/example-1.js
@@ -0,0 +1,15 @@
+/**
+ * When performing operations with decimal numbers, 
+ * there may be residuals due to rounding
+**/
+console.log(0.1 + 0.2);
+// expect output: 0.30000000000000004
+
+console.log(666.7 - 666.6);
+// expect output: 0.10000000000002274
+
+console.log(33.3 * 3);
+// expect output: 99.89999999999999
+
+console.log(12.2 / 0.1);
+// expect output: 121.99999999999999

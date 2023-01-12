07484445b1b755b6c81a7016666dc6ce75fb2fc8
Ederson Macedo <ederson.mos@gmail.com>
2023-01-12 09:21:59 -0300

    Created example propertys method

---
+++ number/math/example-1.js
@@ -0,0 +1,23 @@
+console.log(Math.E); // Euler's number, the base of natural logarithms.
+// expect output: 2.718281828459045
+
+console.log(Math.LN10); // Natural logarithm of 10.
+// expect output: 2.302585092994046
+
+console.log(Math.LN2); // Natural logarithm of 2
+// expect output: 0.6931471805599453
+
+console.log(Math.LOG10E); // Base 10 logarithm of e
+// expect output: 0.4342944819032518
+
+console.log(Math.LOG2E); // Base 2 logarithm of e
+// expect output: 1.4426950408889634
+
+console.log(Math.PI); // Ratio of the circumference of a circle to its diameter
+// expect output: 3.141592653589793
+
+console.log(Math.SQRT1_2); // Square root of 1/2 
+// expect output: 0.7071067811865476
+
+console.log(Math.SQRT2); // Square root of 2
+// expect output: 1.4142135623730951

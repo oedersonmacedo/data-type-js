d2b6b063b81645bcf6137b9e2dd07a4c1f07a3e0
Ederson Macedo <ederson.mos@gmail.com>
2023-01-12 09:44:51 -0300

    Created example methods in Math

---
+++ number/math/example-2.js
@@ -0,0 +1,35 @@
+console.log(Math.abs(10));
+// expect output: 10
+
+console.log(Math.abs(-10));
+// expect output: 10
+
+console.log(Math.ceil(1.1));
+// expect output: 2
+
+console.log(Math.ceil(-1.1));
+// expect output: -1
+
+console.log(Math.floor(9.9));
+// expect output: 9
+
+console.log(Math.floor(-9.9));
+// expect output: -10
+
+console.log(Math.round(4.5));
+// expect output: -5
+
+console.log(Math.round(-4.5));
+// expect output: -4
+
+console.log(Math.sign(5));
+// expect output: 1
+
+console.log(Math.sign(-5));
+// expect output: -1
+
+console.log(Math.trunc(2.3));
+// expect output: 2
+
+console.log(Math.trunc(-2.3));
+// expect output: -2

eccb37cf7b020fa3d937ec2690781dbf24aea77d
Ederson Macedo <ederson.mos@gmail.com>
2023-01-12 09:50:22 -0300

    Created example methods in Math

---
+++ number/math/example-3.js
@@ -0,0 +1,26 @@
+console.log(Math.cbrt(8));
+// expect output: 2
+
+console.log(Math.cos(Math.PI/3));
+// expect output: 0.5000000000000001
+
+console.log(Math.exp(1));
+// expect output: 2.718281828459045
+
+console.log(Math.hypot(3,4));
+// expect output: 5
+
+console.log(Math.log(1));
+// expect output: 0
+
+console.log(Math.pow(2,10));
+// expect output: 1024
+
+console.log(Math.sin(Math.PI/2));
+// expect output: 1
+
+console.log(Math.sqrt(4));
+// expect output: 2
+
+console.log(Math.tan(Math.PI/4));
+// expect output: 0.9999999999999999

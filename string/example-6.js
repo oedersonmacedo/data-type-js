0418215c157efb7efd8073aa8e257ee4ee34af20
Ederson Macedo <ederson.mos@gmail.com>
2023-01-13 07:19:45 -0300

    Example created line break

---
+++ string/example-6.js
@@ -0,0 +1,11 @@
+let daysOfWeek = "0 - Sun\n1 - Mon\n2 - Tue\n3 - Wed\n4 - Thu\n5 - Fri\n6 - Sat";
+console.log(daysOfWeek);
+/** Expect output:
+0 - Sun
+1 - Mon
+2 - Tue
+3 - Wed
+4 - Thu
+5 - Fri
+6 - Sat
+ */

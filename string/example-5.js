0032e04b163650035cbe5a6af4d574cf92a8d6bf
Ederson Macedo <ederson.mos@gmail.com>
2023-01-13 07:15:32 -0300

    Example created character and needs to be escaped

---
+++ string/example-5.js
@@ -0,0 +1,8 @@
+console.log("Each constructor is a function that has a property named \"prototype\" that is used to implement prototype-based inheritance and shared properties.");
+// expect output: Each constructor is a function that has a property named "prototype" that is used to implement prototype-based inheritance and shared properties.
+
+console.log('An object\'s prototype chain should have finite length.');
+// expect output: An object's prototype chain should have finite length.
+
+console.log(`The proper way to cause a line terminator code point to be part of the String value of a string literal is to use an escape sequence such as \\n or \\u000A`);
+// expect output: The proper way to cause a line terminator code point to be part of the String value of a string literal is to use an escape sequence such as \n or \u000A

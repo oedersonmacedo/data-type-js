c8f88fb851fdc7695c65e9ba594f9083f4378e69
Ederson Macedo <ederson.mos@gmail.com>
2023-01-13 06:45:51 -0300

    Created comments of expect output

--- string/example-2.js
+++ string/example-2.js
@@ -1,3 +1,8 @@
 console.log('JavaScript' === "JavaScript");
+// expect output: true
+
 console.log('JavaScript' === `JavaScript`);
+// expect output: true
+
 console.log("JavaScript" === `JavaScript`);
+// expect output: true

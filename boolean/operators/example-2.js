036040b89db466e48e740c7a8ffcfc6a56ae502a
Ederson Macedo <ederson.mos@gmail.com>
2023-01-14 09:56:38 -0300

    Created example operantor equality ande strict equaility

---
+++ boolean/operators/example-2.js
@@ -0,0 +1,22 @@
+//don't confuse equality and strict inequality 
+// with strict equality and strict inequality
+
+// Equality (==) and Inequality(!=)
+console.log('1' == 1);
+// expect output: true
+
+console.log('1' != 1);
+// expect output: false
+
+console.log(null == undefined);
+// expect output: true 
+
+// Strict equality(===) and Strict inequality (!==)
+console.log('1' === 1);
+// expect output: false
+
+console.log('1' !== 1);
+// expect output: true
+
+console.log(null === undefined);
+// expect output: false
---
+++ boolean/operators/example-3.js
@@ -0,0 +1,23 @@
+console.log(0 === '');
+//expect output: false
+
+console.log(0 === '0');
+//expect output: false
+
+console.log(false === undefined);
+//expect output: false
+
+console.log(false === null);
+//expect output: false
+
+console.log(null === undefined);
+//expect output: false
+
+console.log(1 === true);
+//expect output: false
+
+console.log(0 === false);
+//expect output: false
+
+console.log(0 === '\n');
+//expect output: false

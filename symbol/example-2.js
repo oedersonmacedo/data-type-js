c858fa09692a301ce77eac4418cbd8daa0e314bf
Ederson Macedo <ederson.mos@gmail.com>
2023-01-14 10:43:58 -0300

    Example created about referenc unique for Symbol

--- symbol/example-2.js
+++ symbol/example-2.js
@@ -1,32 +1,8 @@
-console.log(Symbol.hasInstance);
-//expect output: Symbol(Symbol.hasInstance)
+console.log(Symbol("a") == Symbol("a"));
+//expect output: false
 
-console.log(Symbol.isConcatSpreadable);
-//expect output: Symbol(Symbol.isConcatSpreadable)
+console.log(Symbol("b") == Symbol("b"));
+//expect output: false
 
-console.log(Symbol.iterator);
-//expect output: Symbol(Symbol.iterator)
-
-console.log(Symbol.match);
-//expect output: Symbol(Symbol.match)
-
-console.log(Symbol.replace);
-//expect output: Symbol(Symbol.replace)
-
-console.log(Symbol.search);
-//expect output: Symbol(Symbol.search)
-
-console.log(Symbol.species);
-//expect output: Symbol(Symbol.species)
-
-console.log(Symbol.split);
-//expect output: Symbol(Symbol.split)
-
-console.log(Symbol.toPrimitive);
-//expect output: Symbol(Symbol.toPrimitive)
-
-console.log(Symbol.toStringTag);
-//expect output: Symbol(Symbol.toStringTag)
-
-console.log(Symbol.unscopables);
-//expect output: Symbol(Symbol.unscopables)
+console.log(Symbol("c") == Symbol("c"));
+//expect output: false

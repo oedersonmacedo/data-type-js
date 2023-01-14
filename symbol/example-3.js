b35adcf14296b83827c3ae5c6e5331c51dac7865
Ederson Macedo <ederson.mos@gmail.com>
2023-01-14 10:44:06 -0300

    Example created functios avalibles for Symbol

---
+++ symbol/example-3.js
@@ -0,0 +1,32 @@
+console.log(Symbol.hasInstance);
+//expect output: Symbol(Symbol.hasInstance)
+
+console.log(Symbol.isConcatSpreadable);
+//expect output: Symbol(Symbol.isConcatSpreadable)
+
+console.log(Symbol.iterator);
+//expect output: Symbol(Symbol.iterator)
+
+console.log(Symbol.match);
+//expect output: Symbol(Symbol.match)
+
+console.log(Symbol.replace);
+//expect output: Symbol(Symbol.replace)
+
+console.log(Symbol.search);
+//expect output: Symbol(Symbol.search)
+
+console.log(Symbol.species);
+//expect output: Symbol(Symbol.species)
+
+console.log(Symbol.split);
+//expect output: Symbol(Symbol.split)
+
+console.log(Symbol.toPrimitive);
+//expect output: Symbol(Symbol.toPrimitive)
+
+console.log(Symbol.toStringTag);
+//expect output: Symbol(Symbol.toStringTag)
+
+console.log(Symbol.unscopables);
+//expect output: Symbol(Symbol.unscopables)

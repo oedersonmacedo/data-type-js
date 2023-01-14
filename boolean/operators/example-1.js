d7ac4f4f0a8156c55664781b1dc6525887ca7863
Ederson Macedo <ederson.mos@gmail.com>
2023-01-14 09:33:41 -0300

    Examples created about operator simple

---
+++ boolean/operators/example-1.js
@@ -0,0 +1,23 @@
+console.log(1 == 2);
+// expect output: false
+
+console.log(10 === 10);
+// expect output: true
+
+console.log('a' != 'b');
+// expect output: true
+
+console.log(3 !== 2);
+// expect output: true
+
+console.log(6 > 7);
+// expect output: false
+
+console.log(5 >= 5);
+// expect output: true
+
+console.log('z' < 'x');
+// expect output: false
+
+console.log('c' <= 'c');
+// expect output: true
